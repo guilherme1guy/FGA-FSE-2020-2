5b21dae66a9f0002a3224e58ae2d9d6b783350f6
Guilherme Guy <guilhermeguy349@gmail.com>
2021-03-16 17:11:29 -0300

    P1: Adds potentiometer manager

---
+++ project_1/src/PotentiometerManager.cpp
@@ -0,0 +1,60 @@
+
+#include "PotentiometerManager.hpp"
+#include "modbus.hpp"
+
+#include <vector>
+
+PotentiometerManager::PotentiometerManager()
+{
+}
+
+float PotentiometerManager::update_temperature(unsigned char operation_code, vector<float> *save_vector)
+{
+
+    vector<unsigned char> data;
+
+    data.push_back(operation_code);
+    data.insert(data.end(), this->REQUEST_BYTES.begin(), this->REQUEST_BYTES.end());
+
+    auto request = ModbusMessage(0x01, 0x23, data);
+
+    ModbusMessage *response = nullptr;
+
+    do
+    {
+        response = ModbusMessage::send(&request);
+    } while (response == nullptr); // keep trying while message is not answered
+                                   // it can happen because of crc or reading errors
+
+    float *received_data = (float *)ModbusMessage::decode(response);
+    float _local_received_data = *received_data; // copy pointer value to local var
+    free(received_data);                         // received data needs to be freed
+
+    // saves received temperature on a temperature vector
+    save_vector->push_back(_local_received_data);
+
+    // request is automatically deleted (cpp mem management)
+    delete response;
+
+    return _local_received_data;
+}
+
+vector<float> PotentiometerManager::get_all_internal_temperatures()
+{
+    return this->internal_temperatures;
+}
+
+vector<float> PotentiometerManager::get_all_potentiometer_temperatures()
+{
+    return this->potentiometer_temperatures;
+}
+
+float PotentiometerManager::get_internal_temperature()
+{
+    return this->update_temperature(0xc1, &this->internal_temperatures);
+}
+
+float PotentiometerManager::get_potentiometer_temperature()
+{
+    return this->update_temperature(0xc2, &this->potentiometer_temperatures);
+}
---
+++ project_1/src/PotentiometerManager.hpp
@@ -0,0 +1,35 @@
+
+#include <vector>
+
+#ifndef PROJECT_1_POTENTIOMETER_HPP
+#define PROJECT_1_POTENTIOMETER_HPP
+
+using namespace std;
+
+class PotentiometerManager {
+
+    // this class manages Potentiometer temperatures collected through modbus
+    // internal temperature
+    // potentiometer temperature
+
+private:
+    const vector<unsigned char> REQUEST_BYTES = {0x31, 0x86};
+    const vector<unsigned char> REQUEST_BYTES = {0x3, 0x1, 0x8, 0x6};
+
+    vector<float> internal_temperatures;
+    vector<float> potentiometer_temperatures;
+
+    float update_temperature(unsigned char operation_code, vector<float>* save_vector);
+
+public:
+    PotentiometerManager();
+
+    vector<float> get_all_internal_temperatures();
+    vector<float> get_all_potentiometer_temperatures();
+
+    float get_internal_temperature();
+    float get_potentiometer_temperature();
+
+};
+
+#endif //PROJECT_1_POTENTIOMETER_HPP
