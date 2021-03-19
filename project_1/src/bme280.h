ca4aabbf4c85a7d67285d3adaac2a37371848a38
Guilherme Guy <guilhermeguy349@gmail.com>
2021-03-19 15:35:54 -0300

    P1: add cpp guard to bme driver

--- project_1/src/bme280.h
+++ project_1/src/bme280.h
@@ -2,7 +2,17 @@
 #ifndef __BME280__
 #define __BME280__
 
+/*! CPP guard */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 int bme280Init(int iChannel, int iAddr);
 int bme280ReadValues(int *T, int *P, int *H);
+
+#ifdef __cplusplus
+}
+#endif /* End of CPP guard */
 
 #endif // __BME280__
