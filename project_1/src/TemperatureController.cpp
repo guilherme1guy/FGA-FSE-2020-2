11af96ac431ec1ba4dfc53c2c8e1c3ca5c03d302
Guilherme Guy <guilhermeguy349@gmail.com>
2021-03-20 15:40:34 -0300

    P1: Adds TemperatureController

---
+++ project_1/src/TemperatureController.cpp
@@ -0,0 +1,192 @@
+#include <thread>
+#include <tuple>
+#include <chrono>
+#include <ctime>
+#include <future>
+#include <math.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "TemperatureController.h"
+#include "LCDManager.hpp"
+#include "Logger.h"
+
+using namespace std;
+
+void TemperatureController::update_lcd()
+{
+}
+
+void TemperatureController::compute_pid()
+{
+    time_t now = time(nullptr);
+    auto delta_time = difftime(now, last_time);
+
+    auto error = this->reference_temperature - this->internal_temperature;
+    this->error_sum += (error * delta_time);              //integral
+    auto delta_error = (error - last_error) / delta_time; // derivative
+
+    // compute pid
+    this->temperature_adjustment = this->kp * error + this->ki * error_sum + this->kd * delta_error;
+
+    // remember values for next run
+    this->last_error = error;
+    this->last_time = now;
+}
+
+void TemperatureController::update_data()
+{
+
+
+    auto f_it = async(&PotentiometerManager::get_internal_temperature, this->potentiometer);
+    auto f_et = async(&PotentiometerManager::get_potentiometer_temperature, this->potentiometer);
+
+    float rt;
+    if (this->reference_source == REFERENCE_FROM_USER)
+    {
+        rt = user_defined_reference_temperature;
+    }
+    else
+    {
+        auto f_data = async(&BMEManager::get_data, this->bme);
+        auto data = f_data.get();
+        rt = get<0>(data);
+    }
+
+    this->internal_temperature = f_it.get();
+    this->external_temperature = f_et.get();
+    this->reference_temperature = rt;
+}
+
+int TemperatureController::clamp(int min, int max, int value){
+
+    int final_value;
+
+    if (value < min){
+        final_value = value;
+    }else if (value > max){
+        final_value = max;
+    }else{
+        final_value = value;
+    }
+
+    return final_value;
+}
+
+tuple<int, int> TemperatureController::get_activation_values(){
+    // returns (resistor %, fan %) based on pid result
+
+    int resistor = 0, fan = 0;
+
+    // if pid is negative it means that the system needs to heat up
+    if (this->temperature_adjustment < 0){
+        // turn on the resistor to heat up
+        // the resistor has a range of 0% to 100%
+        resistor = this->clamp(0, 100, (int) round(this->temperature_adjustment * -1)
+    }else{
+        // turn on the fan to cool down
+        // the fan has a range of 40% to 100%
+        resistor = this->clamp(40, 100, (int) round(this->temperature_adjustment * -1));
+    }
+
+    return make_tuple(resistor, fan);
+}
+
+TemperatureController::TemperatureController()
+{
+
+    this->potentiometer = new PotentiometerManager();
+    this->bme = new BMEManager();
+
+    this->reference_source = REFERENCE_FROM_SENSOR;
+
+    // populate initial data
+    this->update_data();
+    this->last_time = time(nullptr);
+}
+
+void TemperatureController::execute_temperature_control(){
+
+    Logger::log_to_screen("Starting temperature control");
+
+    // each iteration represents a second
+    // every 1 seconds:
+        // - update the lcd
+        // - run pid
+    // every 2 seconds:
+        // - save csv with: datetime, internal temperature, external temperature, user temeprature, resistor %, fan %)
+    
+    bool save_csv = false;
+
+    while (this->execute)
+    {
+        Logger::log_to_screen("Temperature control iteration");
+
+        update_data();
+        compute_pid();
+
+        auto activation_percentages = this->get_activation_values();
+
+        //TODO: take action based on pid
+
+        if (save_csv){
+            save_csv = false;
+
+            stringstream csv_log;
+
+            csv_log << "\"";
+            csv_log << this->last_time;
+            csv_log << "\", ";
+            csv_log << this->internal_temperature;
+            csv_log << ", ";
+            csv_log << this->external_temperature;
+            csv_log << ", ";
+            csv_log << this->reference_temperature;
+            csv_log << ", ";
+            csv_log << get<0>(activation_percentages);
+            csv_log << ", ";
+            csv_log << get<1>(activation_percentages);
+            csv_log << "\n";
+
+            Logger::log_to_file(csv_log.str());
+
+        }else{
+            save_csv = true;
+        }
+       
+        std::this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+    
+}
+
+void TemperatureController::start(){
+
+    // ignore if already in execution
+    if (this->execute) return;
+
+    this->execute = true;
+    this->temperature_control_thread = new thread(&TemperatureController::execute_temperature_control, this);
+
+}
+
+void TemperatureController::end(){
+
+    this->execute = false;
+    this->temperature_control_thread->join();
+
+}
+
+void TemperatureController::set_reference_temperature(float temperature)
+{
+
+    if (temperature >= 0)
+    {
+        this->user_defined_reference_temperature = temperature;
+        this->reference_source = REFERENCE_FROM_USER;
+    }
+    else
+    {
+        this->reference_source = REFERENCE_FROM_SENSOR;
+    }
+}
---
+++ project_1/src/TemperatureController.h
@@ -0,0 +1,77 @@
+#ifndef PROJECT_1_TEMPERATURECONTROLLER_H
+#define PROJECT_1_TEMPERATURECONTROLLER_H
+
+#include <chrono>
+#include <ctime>
+#include <thread>
+#include <tuple>
+
+
+#include "PotentiometerManager.hpp"
+#include "BMEManager.hpp"
+#include "LCDManager.hpp"
+
+using namespace std;
+
+class TemperatureController{
+
+private:
+    
+    // PID related attributes
+    float internal_temperature; // input for pid
+    float external_temperature;
+    float reference_temperature; // target pid temperature
+    
+    // used for integral and derivative calculations
+    float error_sum, last_error;
+
+    // constants used for pid calculations
+    float kp = 5.0, ki = 1.0, kd = 5.0;    
+
+    // result of pid
+    float temperature_adjustment;
+
+    // last time pid was executed (used to get time delta)
+    time_t last_time;
+
+    void compute_pid();
+    void update_data();
+
+    // end of PID part
+
+    int reference_source; // get from user (static) or from potentiometer (every 1 second)
+    float user_defined_reference_temperature;
+    
+    static const int REFERENCE_FROM_SENSOR = 1;
+    static const int REFERENCE_FROM_USER = 2;
+
+    
+
+    PotentiometerManager* potentiometer;
+    BMEManager* bme;
+    LCDManager* lcd;
+
+    void update_lcd();
+
+    int clamp(int min, int max, int value);
+
+    // returns (resistor %, fan %) based on pid result
+    tuple<int, int> get_activation_values();
+
+    bool execute = false;
+    thread *temperature_control_thread;
+    void execute_temperature_control();
+
+
+public:
+
+    TemperatureController();
+
+    void start();
+    void end();
+
+    void set_reference_temperature(float temperature); // if set to -1, use potentiometer
+};
+
+
+#endif //PROJECT_1_TEMPERATURECONTROLLER_H
