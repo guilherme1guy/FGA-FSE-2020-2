cf80dffc063b70135e0b4f07f052bc7e14f26351
Guilherme Guy <guilhermeguy349@gmail.com>
2021-05-10 11:44:11 -0300

    P3: Project Init

---
+++ project_3/client/main/main.c
@@ -0,0 +1,21 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "dht11.h"
+
+void app_main()
+{
+    DHT11_init(GPIO_NUM_4);
+
+    while (1)
+    {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
